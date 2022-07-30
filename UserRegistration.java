a814a67ed2721a793b20d899f419aca3cbb92734
chutki12 <priyankachaudhary2068>
2022-07-30 21:44:10 +0530

    ADDED : This Code For To Check For Valid First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,11 +1,46 @@
 package com.bridgelabz.userRegistrationRegex;
 
-public class UserRegistration {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	public static void main(String[] args) {
+
+public class UserRegistration {
+	 
+       public static void checkValidFirstName(String firstName) {
 		
-		System.out.println(" Welcome To User Registration Program ");
+		     boolean isFirstName;
+		     String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
+		     Pattern pattern = Pattern.compile(firstNameRegex);
+		     if (firstName == null) {
+		    	 isFirstName = false;
+		     }
+		     Matcher matcher = pattern.matcher(firstName);
+		     isFirstName =  matcher.matches();
+        
+		     if(isFirstName)
+		    	 System.out.println(firstName+" is a Valid First Name");
+		     else
+		    	 System.err.println(firstName+" is an Invalid First Name");
+		     
+	}
+	
+	
+       public static void main(String[] args) {
+		
+		  System.out.println(" Welcome To User Registration Program ");
+		  
+		  Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("Enter Your First Name :");
+			String firstName = scanner.next();
+			checkValidFirstName(firstName);
+			
+			scanner.close();
+			
+		}
 
 	}
 
-}
+	
+

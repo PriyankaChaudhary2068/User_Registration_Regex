0c59c4e2fb818aa14d527cd861d9dadce038145f
chutki12 <priyankachaudhary2068>
2022-07-31 12:10:56 +0530

    ADDED: This Code For To Check Valid Password Should Have Atleast One Numeric Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -81,21 +81,19 @@ public class UserRegistration {
          public static void checkValidPassword(String password) {
 
      		boolean isPassword;
-     		String passwordRegex = "^(?=.*[A-Z])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
-     		Pattern pattern = Pattern.compile(passwordRegex);
+     		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
+     		Pattern patternObject = Pattern.compile(passwordRegex);
      		if (password == null) {
      			isPassword = false;
      		}
-     		Matcher matcher = pattern.matcher(password);
-     		isPassword = matcher.matches();
+     		Matcher matcherObject = patternObject.matcher(password);
+     		isPassword = matcherObject.matches();
 
      		if (isPassword)
      			System.out.println(password + " is a Valid  Password\n");
      		else
      			System.out.println(password + " is an Invalid Password");
      	}
-     	             
-     	
 
          
           public static void main(String[] args) {
